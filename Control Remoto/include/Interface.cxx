7ce793efcf00a64978937f7ae0d2860c1d91c1d3
G4BY <gabyguereta@gmail.com>
2023-03-15 23:01:39 -0300

    Cambiazo de Display
    Cambiamos de Display TFT 2.2" a OLED 1.3"
    
    Co-Authored-By: SCH <96535843+0kmsch@users.noreply.github.com>
    Co-Authored-By: schwartzbart <127200921+schwartzbart@users.noreply.github.com>

---
+++ Control Remoto/include/Interface.cxx
@@ -0,0 +1,27 @@
+#include "Interface.h"
+
+
+Adafruit_SSD1306 oled
+
+
+class Display {
+
+private:
+
+
+
+public:
+
+    void Display(Adafruit_SSD1306 oled_ptr){
+
+        this->oled = oled_ptr;
+
+    }
+
+    struct hub{
+
+
+
+    }
+
+};
--- Control Remoto/src/main.cpp
+++ Control Remoto/src/main.cpp
@@ -1,92 +1,21 @@
-#include <Arduino.h>
-#include <SPI.h>
-#include <SD.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_ILI9341.h>
+#include "Interface.h"
 
-#define TFT_DC 9
-#define TFT_CS 10
+Adafruit_SSD1306 AdafruitOled_Object(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+Display oled(AdafruitOled_Object)
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+void setup(){
 
-  tft.begin();
 
-  tft.setCursor(26, 120);
-  tft.setTextColor(ILI9341_RED);
-  tft.setTextSize(3);
-  tft.println("Hello, TFT!");
-
-  tft.setCursor(20, 160);
-  tft.setTextColor(ILI9341_GREEN);
-  tft.setTextSize(2);
-  tft.println("I can has colors?");
-
-  tft.fillScreen(0x00); // PONGO LA PANTALLA EN NEGRO COMO PARA CLEREAR EL DISPLAY
-
-  tft.setCursor(26, 120);
-  tft.setTextColor(ILI9341_RED);
-  tft.setTextSize(3);
-  tft.println("Prendiendo");
-
-  tft.setCursor(20, 160);
-  tft.setTextColor(ILI9341_BLUE);
-  tft.setTextSize(2);
-  tft.println("Proyecto pensado \n y fabricado por:");
-
-  tft.setCursor(100, 200);
-  tft.setTextColor(ILI9341_WHITE);
-  tft.setTextSize(1);
-  tft.println("Gabriel Guereta");
-
-  tft.setCursor(100, 220);
-  tft.setTextColor(ILI9341_WHITE);
-  tft.setTextSize(1);
-  tft.println("Joaquin Torres");
-
-  tft.setCursor(100, 240);
-  tft.setTextColor(ILI9341_WHITE);
-  tft.setTextSize(1);
-  tft.println("Melvin Schwartzbart");
 
 
 }
 
-void loop() {
-  
-if(SD.begin()){
 
-  Serial.println("Memoria SD detectada");
-
-  tft.setCursor(70, 20);
-  tft.setTextColor(ILI9341_RED);
-  tft.setTextSize(1);
-  tft.println("Memoria SD detectada");
-
-  Serial.println("  Crear Perfil");
-    
-  if(!Serial.available()){
-
-
-  }
-  //LUEGO AGREGAR SI QUIERO BORRAR UN PERFIL
+void loop(){
 
 
 
-  }
-  else {
-
-    Serial.println("Ingrese memoria SD");
-
-    tft.setCursor(70, 20);
-    tft.setTextColor(ILI9341_RED);
-    tft.setTextSize(1);
-    tft.println("Ingrese memoria SD");
-
-  }
 
 
 }
