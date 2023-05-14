81e8d1ec70739ecaeb05874a8b9a739bfe73fb43
G4BY <gabyguereta@gmail.com>
2023-05-13 21:12:13 -0300

    Cursor
    Una creación rápida del Writter hasta que Joaco desarrolle el pensado para el proyecto, por lo tanto se usara este para la versión Pre-Alpha, habría que desarrollar la funcion que retorne el String. pero la base y interfaz del Writter esta rápidamente hecha, documentación de el Writter hecho en el Discord y fotos de como se ve.

--- Control Remoto/include/Cursor.cxx
+++ Control Remoto/include/Cursor.cxx
@@ -121,8 +121,8 @@ void CursorV2::showCurrentPage() {
 }
 
 char* CursorV2::getSelectedOption() {
-  showCurrentPage();
   while (true) {
+    showCurrentPage();
     if (digitalRead(UP_BUTTON_PIN) == LOW) {
       currentIndex--;
       if (currentIndex < 0) {
@@ -172,3 +172,96 @@ char* CursorV2::getSelectedOption() {
   }
 }
 
+Writter::Writter(Adafruit_SH1106G& object) : display(object){}
+
+void Writter::Graphics(void){
+  // Clear the buffer
+  display.clearDisplay();
+  display.display();
+  
+  // Display filled in rect in the top section of the display when To_Transfer would be output
+  display.fillRect(0, 0, 128, 15, SH110X_INVERSE);
+  display.drawRect(110, 2, 16, 12, SH110X_BLACK);
+  display.setTextSize(1);            
+  display.setTextColor(SH110X_BLACK);  
+  display.setCursor(113,4); 
+  display.println("OK");
+  display.display();
+  // Display Letter Board 3 rows 9 character in each row 
+  display.setTextSize(2);            
+  display.setTextColor(SH110X_WHITE);       
+  for (int j=0; j<3;j++){
+    for (int i=0; i<9;i++){
+       display.setCursor(i*12+2*i+1,j*16+17);           
+       display.println(Letters[j][i]);
+       delay(10);
+     display.display();
+    }
+  }
+  // Highlight character A by displaying Inverse rect at first position
+  display.fillRect(0, 16, 12, 16, SH110X_INVERSE);
+  display.display();
+}
+
+void Writter::Highlight_letter(int X, int X_Old, int Y, int Y_Old){
+  // When position changes 
+  // Draw the inverse rect in the Old_pos to deactivate  the highlight in the old spot
+  // Draw the inverse rect to Highlite the new spot
+  // Displaying Inverse rect in a new position to highlight
+  display.fillRect(X*12+2*X, Y*16 +16, 12, 16, SH110X_INVERSE);
+  // Displaying Inverse rect in the old positon to unhighlight
+  display.fillRect(X_Old*12+2*X_Old, Y_Old*16 +16, 12, 16, SH110X_INVERSE);
+  display.display();
+}
+
+void Writter::keyboard(void) {
+  Key_read =analogRead(Keyboard);
+  
+  Key_pressed=true;
+  if ((buttonState(LEFT_BUTTON_PIN) == LOW) and Old_X>0){ New_X=Old_X-1; delay(300);}
+  if ((buttonState(RIGHT_BUTTON_PIN) == LOW) and Old_X<9){ New_X=Old_X+1; delay(300);}
+  if ((buttonState(UP_BUTTON_PIN) == LOW) and Old_Y>-1){ New_Y=Old_Y-1; delay(300);}
+  if ((buttonState(DOWN_BUTTON_PIN) == LOW) and  Old_Y<2 ){ New_Y=Old_Y+1; delay(300);}
+  if (buttonState(RIGHT_BUTTON_PIN) == LOW) {
+  delay(300);
+   if (New_Y!=-1){
+     To_Transmit=To_Transmit + Letters[New_Y][New_X];
+     To_Transmit_Length++;
+     display.setTextSize(1);
+     display.setCursor(3,1);
+     display.setTextColor(SH110X_BLACK);
+     display.fillRect(0, 0, 100, 15, SH110X_WHITE);
+     display.println(To_Transmit);
+     display.display();
+   }
+   else{
+    for (int i=1;i<9;i++) {
+      display.fillRect(0, 0, 128, 15, SH110X_INVERSE);
+      delay(300);
+      display.display();
+    }
+   }
+  }     
+  if (New_Y==-1 and Old_Y==0){
+      display.fillRect(110, 2, 16, 12, SH110X_INVERSE); 
+      display.fillRect(Old_X*12+2*Old_X, Old_Y*16 +16, 12, 16, SH110X_INVERSE); 
+  }
+  if (New_Y==0 and Old_Y==-1){
+      display.fillRect(110, 2, 16, 12, SH110X_INVERSE); 
+      display.fillRect(New_X*12+2*New_X, New_Y*16 +16, 12, 16, SH110X_INVERSE);
+      Prev_Key_read=Key_read;
+      Old_X=New_X;
+      Old_Y=New_Y;;
+  }
+  if ((Old_X!=New_X or Old_Y!=New_Y) and Old_Y!=-1 ){
+      if (New_Y!=-1 )Highlight_letter (New_X,Old_X,New_Y,Old_Y);
+      Old_X=New_X;
+      Old_Y=New_Y;
+  }
+  
+  display.display();
+  Prev_Key_read=Key_read; 
+
+}
+
+String Writter::stringFinished(void){ return To_Transmit;}
--- Control Remoto/include/Cursor.h
+++ Control Remoto/include/Cursor.h
@@ -11,13 +11,45 @@
 #define CURSOR_H
 
 #include "Interface.h"
+#include <stdint.h>
 #include <Adafruit_SH110X.h>
 
-/*! @brief Cursor que se desplaza a traves del display
+//CursorV2 Macros
+#define UP_BUTTON_PIN PIN::Buttons::UP
+#define DOWN_BUTTON_PIN PIN::Buttons::DOWN
+#define ENTER_BUTTON_PIN PIN::Buttons::ENTER
+#define BACK_BUTTON_PIN PIN::Buttons::BACK
+
+
+//Writter Macros
+#ifndef buttonState_function_Macro
+  #define buttonState_function_Macro
+  /*! @brief Estado logico del pin de la placa de desarrollo
+    @param PIN_BUTTON 
+           Pin de la placa de desarrollo
+    @returns Estado logico del pin de la placa de desarrollo */
+  #define buttonState(PIN_BUTTON) digitalRead(PIN_BUTTON) 
+#endif
+#define UP_BUTTON_PIN PIN::Buttons::UP
+#define DOWN_BUTTON_PIN PIN::Buttons::DOWN
+#define LEFT_BUTTON_PIN PIN::Buttons::LEFT
+#define RIGHT_BUTTON_PIN PIN::Buttons::RIGHT
+#define ENTER_BUTTON_PIN PIN::Buttons::ENTER
+#define BACK_BUTTON_PIN PIN::Buttons::BACK
+// Oled display size
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+// Mario's Ideas
+// Text input using OLED display an 5 Key Keyboard
+
+
+/*! \deprecated Cursor nuevo desarrollado llamado: CursorV2
+    @brief Cursor que se desplaza a traves del display
     @note Posee de dos constructores
     @note 1- Para desplazarse entre los menus del display
     @note 2- Para la creacion de strings que recibe del usuario
-    \deprecated Cursor nuevo desarrollado llamado: CursorV2*/
+*/
+[[deprecated("Se reemplazo por CursorV2, esta en desuso esta clase")]] 
 class Cursor {
 
 private:
@@ -43,12 +75,6 @@ public:
 };
 
 /*! @brief Cursor que se desplaza a traves del display (desarrollado v2)*/
-
-#define UP_BUTTON_PIN PIN::Buttons::UP
-#define DOWN_BUTTON_PIN PIN::Buttons::DOWN
-#define ENTER_BUTTON_PIN PIN::Buttons::ENTER
-#define BACK_BUTTON_PIN PIN::Buttons::BACK
-
 class CursorV2 {
   private:
     char** options;
@@ -57,14 +83,73 @@ class CursorV2 {
     int totalPages;
     int currentPage;
   public:
+
+    /*! @brief Constructor
+        @param menuOptions Doble puntero que apunta a array de punteros que apuntan a strings (las opciones)
+        @param display Recibe referencia o puntero al objeto del display
+    */
     CursorV2(char** menuOptions, Adafruit_SH1106G* display);
 
+    /*! @brief Obtiene el numero total strings
+        @returns Numero total de punteros que hay dentro del array
+    */
     int getNumberOfOptions();
 
+    /*! @brief Pagina/apartado en el que se encuentra el cursor
+        @note Lo muestra en el screen Oled
+    */
     void showCurrentPage();
 
+    /*! @brief Inicia bucle infinito 
+        @returns
+        @note Lo muestra en el screen Oled
+    */
     char* getSelectedOption();
 };
+
+
+
+/*! @brief Cursor que se desplaza a traves del display (desarrollado v2)*/
+class Writter{
+
+  private:
+  //Referencia al objeto del display
+    Adafruit_SH1106G& display;
+  //Potentiometer PIN A1
+    int Keyboard=A7;
+  // Variables capturing current and newly calculated position on the letter board (9x3 - 27 postions)
+    int New_X=0;
+    int Old_X=0;
+    int New_Y=0;
+    int Old_Y=0;
+  // Variable capturing output from Keyboard pin (Values 0 1023)
+    int Key_read=0;
+    int Prev_Key_read=1023;
+    boolean Key_pressed=false; 
+  // String variable holding the text to transmit
+    String To_Transmit="";
+  // Length of the text to transmit
+    int To_Transmit_Length=0;
+
+
+  // Used for displaying Leter board
+  char Letters[3][10]={"ABCDEFGHI",
+                       "JKLMNOPQR",
+                       "STUVWXYZ_" };
+  
+  void Graphics(void);
+  void Highlight_letter(int X, int X_Old, int Y, int Y_Old);
+  void keyboard(void);
+
+
+  public:
+
+  Writter(Adafruit_SH1106G& display);
+
+  String stringFinished(void);
+
+};
+
 
 
 #endif //Cursor_h
